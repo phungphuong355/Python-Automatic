6aca72bf196251c28087772998a4b964e9e3b28f
Phuong Phung Tan <phungphuong355@gmail.com>
2023-08-19 22:50:13 +0700

    update file

--- 3_regex.py
+++ 3_regex.py
@@ -27,7 +27,6 @@ print(re.search(r"aza", "plaza"))  # <re.Match object; span=(2, 5), match='aza'>
 print(re.search(r'^x', "xenon"))  # <re.Match object; span=(0, 1), match='x'>
 print(re.search(r'^x', "n xenon"))  # None
 
-
 print(re.search(r'p.ng', "penguin"))  # <re.Match object; span=(0, 4), match='peng'>
 print(re.search(r'p.ng', "clapping"))  # <re.Match object; span=(4, 8), match='ping'>
 print(re.search(r'p.ng', "sponge"))  # <re.Match object; span=(1, 5), match='pong'>
@@ -61,4 +60,70 @@ print(re.search(r'o+l+', 'boil'))  # None
 print(re.search(r'p?each', "To each their own"))  # <re.Match object; span=(3, 7), match='each'>
 print(re.search(r'p?each', "I like peaches"))  # <re.Match object; span=(7, 12), match='peach'>
 
+# Escaping Characters
+# \
+print(re.search(r'\.com', 'welcome'))  # None
+print(re.search(r'\.com', 'domain.com'))  # <re.Match object; span=(8, 12), match='.com'>
+# \w \d \s \b
+print(re.search(r'\w*', 'This is an example'))  # <re.Match object; span=(0, 4), match='This'>
+print(re.search(r'\w*', 'And_this_is_another'))  # <re.Match object; span=(0, 19), match='And_this_is_another'>
 
+# Regular Expressions in Action
+print(re.search(r'A.*a', "Azerbaijan"))  # <re.Match object; span=(0, 9), match='Azerbaija'>
+print(re.search(r'^A.*a$', "Azerbaijan"))  # None
+print(re.search(r'^A.*a$', "Australia"))  # <re.Match object; span=(0, 9), match='Australia'>
+pattern = r'^[a-zA-Z_][a-zA-Z0-9_]*$'
+print(re.search(pattern, '_this_is_a_valid_variable_name'))
+# <re.Match object; span=(0, 30), match='_this_is_a_valid_variable_name'>
+print(re.search(pattern, "this isn't a valid a variable"))  # None
+print(re.search(pattern, 'my_variable1'))
+# <re.Match object; span=(0, 12), match='my_variable1'>
+print(re.search(pattern, '2my_variable1'))  # None
+
+
+# Capturing Groups
+def rearrange_name(name):
+    result = re.search(r"^([\w .-]*), ([\w \.-]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+
+name = rearrange_name("Kennedy, John F.")
+print(name)
+
+# More on Repetition Qualifiers
+print(re.search(r"[a-zA-Z]{5}", "a ghost"))  # <re.Match object; span=(2, 7), match='ghost'>
+print(re.search(r"[a-zA-Z]{5}", "a scary ghost appeared"))  # <re.Match object; span=(2, 7), match='scary'>
+print(re.findall(r"[a-zA-Z]{5}", "a scary ghost appeared"))  # ['scary', 'ghost', 'appea']
+print(re.findall(r"\b[a-zA-Z]{5}\b", "a scary ghost appeared"))  # ['scary', 'ghost']
+print(re.findall(r"\w{5,10}", "I really like strawberries"))  # ['really', 'strawberri']
+print(re.findall(r"\b\w{5,10}\b", "I really like strawberries"))  # ['really']
+print(re.findall(r"\w{5,}", "I really like strawberries"))  # ['really', 'strawberries']
+print(re.findall(r"\w{,20}", "I really like strawberries"))  # ['I', '', 'really', '', 'like', '', 'strawberries', '']
+print(re.findall(r"s\w{,20}", "I really like strawberries"))  # ['strawberries']
+
+
+# Extracting a PID Using regexes in Python
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]: ([A-Z]\w*)"
+    result = re.search(regex, log_line)
+    if result is None:
+        return None
+    return "{} ({})".format(result[1], result[2])
+
+
+print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"))  # 12345 (ERROR)
+print(extract_pid("99 elephants in a [cage]"))  # None
+print(extract_pid("A string that also has numbers [34567] but no uppercase message"))  # None
+print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup"))  # 67890 (RUNNING)
+
+# Splitting and Replacing
+print(re.split(r'[.?!]', "One sentence. Another one? And the last one!"))
+# ['One sentence', ' Another one', ' And the last one', '']
+print(re.split(r'([.?!])', "One sentence. Another one? And the last one!"))
+# ['One sentence', '.', ' Another one', '?', ' And the last one', '!', '']
+print(re.sub(r'[\w.%+-]+@[\w.-]+', "[READACTED]", "Received an email for go_nuts95@my.example.com"))
+# Received an email for [READACTED]
+print(re.sub(r'^([\w .-]*), ([\w .-]*)$', r'\2 \1', 'Lovelace, Ada'))
+# Ada Lovelace
